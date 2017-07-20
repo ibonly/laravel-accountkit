eec1a775072c91845c603cfc709b29ea6d3b3e12
Adeniyi Ibraheem <ibonly01@gmail.com>
2017-07-20 20:40:36 +0100

    Add token and refresh_interval to return data

--- src/FacebookAccountKit.php
+++ src/FacebookAccountKit.php
@@ -9,25 +9,28 @@ class FacebookAccountKit extends AccountKit
         return $this->getData($code)['id'];
     }
 
-    public function getRefreshInterval($code)
+    public function getAppToken($code)
     {
-        return $this->getData($code)->token_refresh_interval_sec;
+        return $this->getData($code);
     }
 
     public function facebookAccountKit($code)
     {
         $accountKitData = $this->data($code);
+        $token = $this->getAppToken($code);
 
         $output = [
             'id' => $accountKitData->id,
             'phoneNumber' => '',
             'email' => '',
+            'token' => $token->access_token,
+            'timeInterval' => $token->token_refresh_interval_sec
         ];
 
         if (array_key_exists('phone', $accountKitData)) {
             $output['phoneNumber'] = $accountKitData->phone->number ?? null;
         }
-        
+
         if (array_key_exists('email', $data)) {
             $output['email'] = $data->email->address ?? null;
         }
